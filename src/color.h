28c98d4f5fb4e47c76d3437ffd82231238ba7d69
Issmale Bekri <issmale@iastate.edu>
2025-09-13 11:41:41 -0500

    anti aliasing

--- src/color.h
+++ src/color.h
@@ -8,10 +8,23 @@ inline double clamp(double x, double min, double max) {
     return x;
 }
 
-inline void write_color(std::ostream& out, const color& pixel) {
+inline void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
+    double r = pixel_color.x();
+    double g = pixel_color.y();
+    double b = pixel_color.z();
 
-    int ir = static_cast<int>(256 * clamp(pixel.x(), 0.0, 0.999));
-    int ig = static_cast<int>(256 * clamp(pixel.y(), 0.0, 0.999));
-    int ib = static_cast<int>(256 * clamp(pixel.z(), 0.0, 0.999));
+    double scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    r = sqrt(r);
+    g = sqrt(g);
+    b = sqrt(b);
+
+    int ir = static_cast<int>(256 * clamp(r, 0.0, 0.999));
+    int ig = static_cast<int>(256 * clamp(g, 0.0, 0.999));
+    int ib = static_cast<int>(256 * clamp(b, 0.0, 0.999));
+
     out << ir << ' ' << ig << ' ' << ib << '\n';
 }
