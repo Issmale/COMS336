fa38ef591e313aae70a2fb4c57ee1df1e5b03a41
Issmale Bekri <issmale@iastate.edu>
2025-09-14 13:16:03 -0500

    added a heloer method

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -18,3 +18,11 @@ inline double random_double() {
 inline double random_double(double min, double max) {
     return min + (max - min) * random_double();
 }
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), random_double(-1,1));
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
